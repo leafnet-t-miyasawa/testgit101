f42d111ef5fbcbf774f4630918b8f590a068c8c0
t.miyasawa <t.miyasawa@leafnet.jp>
2023-12-13 11:31:32 +0900

    20231213_101

--- test/test.php
+++ test/test.php
@@ -1,10 +1,7 @@
 <?
-<<<<<<< HEAD
 phpinfo(); // てすとてすと
 echo "develope test";
-=======
 phpinfo(); // てすとてすとtestsetset
 echo "----- test102";
 echo "----- test 101  @ br_test3";
 echo "---- test20200602 101";
->>>>>>> branch 'develope' of https://github.com/tomoatgit/testgit101
